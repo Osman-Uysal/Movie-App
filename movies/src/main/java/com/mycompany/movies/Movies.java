c3a573dd6e3633045e9ea2e6afffdc1ba9501b7b
Osman Uysal <93253598+Osman-Uysal@users.noreply.github.com>
2023-01-24 03:44:32 +0000

    Add files via upload

---
+++ movies/src/main/java/com/mycompany/movies/Movies.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.movies;
+
+/**
+ *
+ * @author osman
+ */
+public class Movies {
+
+    public static void main(String[] args) {
+        MoviesGUI myGUI = new MoviesGUI();
+        myGUI.setVisible(true);
+    }
+}
